f6d81e013641c02b8c761f4b8d76148c5e9de4c1
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2021-04-13 21:22:59 -0700

    auto format

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,17 @@ import Test1 from './Test';
 class App extends React.Component<{}, {}> {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
           </p>
           <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
           >
             Learn React
           </a>
