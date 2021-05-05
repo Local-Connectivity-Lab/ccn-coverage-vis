e4b350a136de55d523eeb742b0f1c928e086a0ab
Chanwut Kittivorawong <30903997+chanwutk@users.noreply.github.com>
2021-05-05 06:31:09 -0700

    connect map type radio to map b8f113e (#2)

--- src/MapSelectionRadio.tsx
+++ src/MapSelectionRadio.tsx
@@ -7,28 +7,57 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
 import 'fontsource-roboto';
 
-export default function MapSelectionRadio() {
+const MAP_TYPE_INDEX = {
+  ping: 1,
+  upload_speed: 1,
+  download_speed: 1,
+} as const;
+export type MapType = keyof typeof MAP_TYPE_INDEX;
 
+function isMapType(m: any): m is MapType {
+  return m in MAP_TYPE_INDEX;
+}
+
+interface MapSelectionRadioProps {
+  mapType: MapType;
+  setMapType: React.Dispatch<React.SetStateAction<MapType>>;
+}
+
+export default function MapSelectionRadio(props: MapSelectionRadioProps) {
   type InputEvent = React.ChangeEvent<HTMLInputElement>;
-  const [mapType, setMapType] = React.useState('signal');
-
   const handleChange = (event: InputEvent) => {
-    setMapType(event.target.value);
-    alert(event.target.value);
+    const _mapType = event.target.value;
+    if (!isMapType(_mapType)) {
+      throw new Error('Invalid map type selection: ' + _mapType);
+    }
+    props.setMapType(_mapType);
   };
 
   return (
     <div className='App'>
       <Container>
-        <FormControl component="fieldset">
-          <Typography variant="overline">
-            Map Type
-          </Typography>
-          <RadioGroup row aria-label="gender" name="gender1" value={mapType} onChange={handleChange}>
-            <FormControlLabel value="signal" control={<Radio />} label="Signal" />
-            <FormControlLabel value="uploadSpeed" control={<Radio />} label="Upload Speed" />
-            <FormControlLabel value="downloadSpeed" control={<Radio />} label="Download Speed" />
-            <FormControlLabel value="traffic" control={<Radio />} label="Traffic" />
+        <FormControl component='fieldset'>
+          <Typography variant='overline'>Map Type</Typography>
+          <RadioGroup
+            row
+            aria-label='gender'
+            name='gender1'
+            value={props.mapType}
+            onChange={handleChange}
+          >
+            {/* remove signal for now because we don't have "signal" in our mock data */}
+            {/* <FormControlLabel value="signal" control={<Radio />} label="Signal" /> */}
+            <FormControlLabel
+              value='upload_speed'
+              control={<Radio />}
+              label='Upload Speed'
+            />
+            <FormControlLabel
+              value='download_speed'
+              control={<Radio />}
+              label='Download Speed'
+            />
+            <FormControlLabel value='ping' control={<Radio />} label='Ping' />
           </RadioGroup>
         </FormControl>
       </Container>
--- src/MeasurementPoint.tsx
+++ src/MeasurementPoint.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Circle } from 'react-leaflet';
-import { ScaleSequential } from 'd3';
 
-interface MeasurementPointProps {
+export interface MeasurementDatum {
   latitude: number;
   longitude: number;
   timestamp: string;
@@ -12,15 +11,19 @@ interface MeasurementPointProps {
   ping: number;
   site: string;
   device_id: number;
-  color: ScaleSequential<string, never>
+}
+
+interface MeasurementPointProps extends MeasurementDatum {
+  color: string;
 }
 
 const MeasurementPoint = (props: MeasurementPointProps) => {
-  return <Circle
-    key={`${props.device_id}-${props.timestamp}`}
-    center={[props.latitude, props.longitude]}
-    color={props.color(props.ping)}
-  ></Circle>;
+  return (
+    <Circle
+      center={[props.latitude, props.longitude]}
+      pathOptions={{ color: props.color, fillColor: props.color }}
+    ></Circle>
+  );
 };
 
 export default MeasurementPoint;
