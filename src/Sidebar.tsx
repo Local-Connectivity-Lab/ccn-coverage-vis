fd0c9bdc66b96b69564b33741c1cfd15685e8be9
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2021-12-06 20:23:23 -0800

    update package

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
-import MultiSelect from 'react-multi-select-component';
+import {MultiSelect} from 'react-multi-select-component';
 import 'fontsource-roboto';
 import sites from './sites.json';
 
