421b71ba1cd8dc91bec71e6f4b5f708eb89a15ee
Mark <sirapop741@gmail.com>
2022-01-04 23:42:15 -0800

    Attempt to fix router v4

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+// import React from 'react';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import Login from './Login';
-// import AdminPortal from './AdminPortal';
+import Login from './Login';
+import AdminPortal from './AdminPortal';
 import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/admin' element={<AdminPortal />} />
+    </Routes>
+  </BrowserRouter>,
   document.getElementById('root'),
 );
