e947d055efa5d220bcc4910188a0a61d2ec6ac65
Mark <sirapop741@gmail.com>
2022-01-04 22:32:20 -0800

    Add .htaccess

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Login from './Login';
+import AdminPortal from './AdminPortal';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/admin' element={<AdminPortal />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
