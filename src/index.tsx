ac34d03baf1caf40a46a08fd149d64c86571c212
Mark <sirapop741@gmail.com>
2022-01-04 21:43:43 -0800

    Try remove React Router

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Login from './Login';
-import AdminPortal from './AdminPortal';
 import './index.css';
 
 ReactDOM.render(
